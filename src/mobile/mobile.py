65b142094b6e6e769e6780031eec00fb244e06eb
Majka w ścianie <95749943+Majka-w-scianie@users.noreply.github.com>
2025-10-08 09:01:16 +0200

    completed webscrapping

--- src/mobile/mobile.py
+++ src/mobile/mobile.py
@@ -10,23 +10,31 @@ def str_cleanup(x: str):
     x = x.replace('[', '').replace(']', '')
     x = x.replace('<td>', '').replace('</td>', '')
     x = x.replace('<tr>', '').replace('</tr>', '')
-    x = x.replace('<tr>', '').replace('</tr>', '')
+    x = x.replace('<tr>', '').replace('</tr>', '').replace('</table>', ';')
+    x = x.replace('<td align="LEFT" bgcolor="#69AADE" class="st1" colspan="4" nowrap="">', ';')
+    x = x.replace('\t', '').replace('\n', '').replace('\r', '')
     ch = 0
     check = 0
     while ch < len(x):
-        temp = 0
-        if x[ch] == '<':
-            check = 1
-        elif x[ch] == '>':  
-            check = 0
-        if check == 1:    
-            temp += 1
-        x = x[:ch] + x[ch + temp:]    
-        ch += 1    
-        print(x)
+        ch = x.index('>') + 1
+        x = x[:x.index('<')] + ', ' + x[x.index('>') + 1:]
+    x = x.replace(';', '; \n')
+    x = x.replace('lekcja, opis, zastępca, uwagi   , ', '')
+    x = x.replace(' -', ',')
+    # print(x)
 
     return x
 
+def get_inf(x: str):
+    x = x[x.index('\n') + 1:]
+    if len(x) > 0:    
+        imiNazw = []
+        lekcja = []
+        klasa = []
+        sala = []
+        zastępca = []
+        uwagi = []
+    return x
 
 def main():
     html = zastepstwa_get()
