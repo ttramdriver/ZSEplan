be04fce241137a44052a03f83c0562ae5ee3ee9d
Majka w ścianie <95749943+Majka-w-scianie@users.noreply.github.com>
2025-10-10 11:27:26 +0200

    lil update

--- src/common/zastepstwa.py
+++ src/common/zastepstwa.py
@@ -9,8 +9,7 @@ def zastepstwa_get():
 def str_cleanup(x: str):
     x = x.replace('[', '').replace(']', '')
     x = x.replace('<td>', '').replace('</td>', '')
-    x = x.replace('<tr>', '').replace('</tr>', '')
-    x = x.replace('<tr>', '').replace('</tr>', '').replace('</table>', ';')
+    x = x.replace('<tr>', '/').replace('</tr>', '').replace('</table>', ';')
     x = x.replace('<td align="LEFT" bgcolor="#69AADE" class="st1" colspan="4" nowrap="">', ';')
     x = x.replace('\t', '').replace('\n', '').replace('\r', '')
     ch = 0
@@ -19,14 +18,17 @@ def str_cleanup(x: str):
         ch = x.index('>') + 1
         x = x[:x.index('<')] + ', ' + x[x.index('>') + 1:]
     x = x.replace(';', '; \n')
-    x = x.replace('lekcja, opis, zastępca, uwagi   , ', '')
+    x = x.replace('/, lekcja, opis, zastępca, uwagi   ', '')
+    x = x.replace(',  ,  ,  ,  /', '')
+    x = x.replace('/,', '/')
     x = x.replace(' -', ',')
     # print(x)
 
     return x
 
-def get_inf(x: str):
+def data_process(x: str):
     x = x[x.index('\n') + 1:]
+    x = x.replace(' ', '')#.replace(' ', '')
     if len(x) > 0:    
         imiNazw = []
         lekcja = []
@@ -34,7 +36,24 @@ def get_inf(x: str):
         sala = []
         zastępca = []
         uwagi = []
-    return x
+        i = 0
+        start = 0
+        end = 0
+        elementIndex = 0
+        while i < len(x):
+            if x[i] == '/':
+                if elementIndex == 0:
+                    end = i
+                    elementIndex = 0
+                    imiNazw.append(x[start:end])
+                    return imiNazw
+            i += 1
+                # elif elementIndex == 1:
+            # if x[i] == ';':
+                # i+=1 
+                # elementIndex = -1
+        
+    # return x
 
 def main():
     html = zastepstwa_get()
@@ -42,10 +61,16 @@ def main():
     zastepstwa = soup.select("table")
     strzastep = str(zastepstwa)
     strzastep = str_cleanup(strzastep)
-    print(strzastep)
-
-    with open('test.txt', 'w') as f:
+    # print(strzastep)
+    
+    with open('zastepstwa.txt', 'w') as f:
         f.write(strzastep)
-
-
+    
+    with open('zastepstwa.txt', 'r') as f:
+        noInternetZastep = f.read()
+    
+    with open('test.txt', 'w') as f:
+        f.write(str(data_process(noInternetZastep)))
+        
+       
 main()
